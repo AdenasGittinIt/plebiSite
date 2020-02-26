adab2a118149b377a6a0078104880070275ef4b7
Adena McGibbon <justadena@gmail.com>
2020-02-25 22:43:32 -0700

    initial description

---
+++ scratchPad.js
@@ -0,0 +1,3 @@
+// List of the members of congress
+// https://api.propublica.org/congress/v1/116/senate/members.json
+// returns an array of objects.  one object per member of congress.  res.results[i].
