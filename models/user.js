104b8a713f36c3466b914251e86e41f551eabe57
Adena McGibbon <justadena@gmail.com>
2020-03-06 19:17:49 -0700

    encryption working

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema,
+bcrypt = require('bcrypt'),
+SALT_WORK_FACTOR = 10;
 
 const usersSchema = new Schema (
   {
@@ -31,6 +33,35 @@ const usersSchema = new Schema (
   },
 )
 
+usersSchema.pre('save', function(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
+});
+
+usersSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', usersSchema);
 
 const User = mongoose.model("User", usersSchema)
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,6 +4,7 @@ const passport = require("../config/passport");
 module.exports = function(app) {
 
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    console.log(req.user)
     res.json(req.user);
   });
 
