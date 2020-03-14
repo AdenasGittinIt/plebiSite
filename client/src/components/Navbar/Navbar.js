a148944c88ed73eba0fc77f112ceb598ddcb4119
Adena McGibbon <justadena@gmail.com>
2020-03-14 14:45:57 -0600

    commit to deploy

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,12 +7,12 @@ const NavBar = () => {
     return (
       <Navbar>
           <a href="/">Home</a>
-          <a href="memberlisting/Senate">Senate</a>
-          <a href="memberlisting/House">House</a>
           <a href="/registration">Register</a>
           <a href="/login">Log In</a>
+          <a href="memberlisting/Senate">Senate</a>
+          <a href="memberlisting/House">House</a>
       </Navbar>
     )
   }
 
-export default NavBar
+export default NavBar 
